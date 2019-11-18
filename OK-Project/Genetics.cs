98f756b039de2e53ebbb35f7d78a93cbaef8cd24
Selethen <lukisd@op.pl>
2019-11-18 09:58:13 +0100

    +Added mutation +Added starting function

---
+++ OK-Project/Genetics.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OK_Project
+{
+    class Genetics
+    {
+        Random rand = new Random();
+        public Graph graph;
+
+        public int[] fitness;
+        public int[,] solutions;
+
+        private void generatePopulation(int amount)
+        {
+            solutions = new int[amount, graph.V];
+            fitness = new int[amount];
+
+            int nodes = graph.V;
+            int colours = 0;
+
+            for (int i = 0; i < nodes; i++)
+            {
+                if (graph.adj[i].Count() > colours)
+                    colours = graph.adj[i].Count();
+            }
+
+            for (int i = 0; i < amount; i++)
+            {
+                Console.Write("Rozwiązanie " + i + ": ");
+                for (int j = 0; j < nodes; j++)
+                {
+                    solutions[i, j] = rand.Next(colours / 4);
+                    Console.Write(solutions[i,j] + ", ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        /**
+         * Function that calculates Fitness Score for given solution[index].
+         * Fitness == 0, means that we have perfect fitness
+         * In this case, as fitness score we used number of edges, that vertices have the same colour.
+         */
+        private void fitnessScore(int index)
+        {
+            fitness[index] = 0;
+            for (int i = 0; i < graph.adj.Length; i++)
+            {
+                foreach (int j in graph.adj[i])
+                {
+                    // Iterates every element in every list
+                    if (solutions[index,i] == solutions[index,j])
+                        ++fitness[index];
+                }
+            }
+            fitness[index] /= 2; // 'cuz calculates twice the same edge
+        }
+
+        /**
+         * Function that select fittest parents from two random pairs of chromosomes.
+         * Used when best fitness greater than 4.
+         */
+        private Tuple<int, int> parentSelect1()
+        {
+            int index1, index2;
+
+            int tempPar1 = rand.Next(fitness.Length);
+            int tempPar2 = rand.Next(fitness.Length);
+
+            if (fitness[tempPar1] > fitness[tempPar2])
+                index1 = tempPar1;
+            else
+                index1 = tempPar2;
+
+            tempPar1 = rand.Next(fitness.Length);
+            tempPar2 = rand.Next(fitness.Length);
+
+            if (fitness[tempPar1] > fitness[tempPar2])
+                index2 = tempPar1;
+            else
+                index2 = tempPar2;
+
+            return new Tuple<int, int>(index1, index2);
+        }
+
+        /**
+        * Function that select two best performing chromosomes
+        * Used when best fitness less than 4.
+        */
+        private Tuple<int, int> parentSelect2()
+        {
+            int index1, index2;
+            index1 = 0;
+            index2 = 0;
+
+            for (int i = 1; i < fitness.Length; i++)
+                if (fitness[index1] > fitness[i])
+                    index1 = i;
+
+            for (int i = 1; i < fitness.Length; i++)
+                if ((fitness[index2] > fitness[i]) && (index1 != i))
+                    index2 = i;
+            
+            return new Tuple<int, int> (index1, index2);
+        }
+
+        /**
+         * Funcion that cross two parents into one child.
+         * I like to call it "reproduction".
+         * Crosspoint is generated at random.
+         */
+        private int[] crossover(int index1, int index2)
+        {
+            int[] child = new int[graph.V];
+
+            int crosspoint = rand.Next(graph.V);
+
+            for (int i = 0; i <= crosspoint; i++)
+                child[i] = solutions[index1, i];
+
+            for (int i = crosspoint + 1; i < graph.V; i++)
+                child[i] = solutions[index2,i];
+
+            return child;
+        }
+
+        /**
+         * Function replacing the least fittest chromosome with new child.
+         * Returns index of child's new position.
+         */
+        private int offspring(int[] result)
+        {
+            int maxFittness = fitness[0];
+            int leastFittestIndex = 0;
+
+            for (int i = 1; i < fitness.Length; ++i)
+            {
+                if (fitness[i] > maxFittness)
+                {
+                    maxFittness = fitness[i];
+                    leastFittestIndex = i;
+                }
+            }
+            
+            for (int i = 0; i < solutions.GetLength(1); i++)
+            {
+                solutions[leastFittestIndex, i] = result[i];
+            }
+
+            return leastFittestIndex;
+        }
+
+        /**
+         * Function which causes mutation of item at [mutationIndex].
+         * Replaces colors of adjacent colors if can.
+         */
+        private void mutation1(int mutationIndex)
+        {
+            LinkedList<int> adjacentColors = new LinkedList<int>(); // empty array of all adjacent colors
+            LinkedList<int> allColors = new LinkedList<int>(); // empty array of all colors
+
+            for (int i = 0; i < graph.V; i++) // fill allColors with maximum number of colors
+                allColors.AddLast(i);
+
+            for (int i = 0; i < graph.V; i++)
+            {
+                adjacentColors.Clear();
+                foreach (int item in graph.adj[i]) // fill adjacentColors
+                    adjacentColors.AddLast(solutions[mutationIndex, item]);
+
+                if (adjacentColors.Contains(solutions[mutationIndex, i]))
+                {
+                    // mutate
+                    LinkedList<int> validColors = new LinkedList<int>();
+                    foreach (int item in allColors)
+                    {
+                        if (!adjacentColors.Contains(item))
+                            validColors.AddLast(item);
+                    }
+
+                    /* To test: (TO DO)
+                     * A) smallest colors from validColors()
+                     * B) random color from validColors()
+                     */
+                    solutions[mutationIndex, i] = validColors.First();
+                }
+            }
+
+        }
+
+        private void mutation2(int mutationIndex)
+        {
+            LinkedList<int> adjacentColors = new LinkedList<int>(); // empty array of all adjacent colors
+            LinkedList<int> allColors = new LinkedList<int>(); // empty array of all colors
+
+            for (int i = 0; i < graph.V; i++) // fill allColors with maximum number of colors
+                allColors.AddLast(i);
+
+            for (int i = 0; i < graph.V; i++)
+            {
+                adjacentColors.Clear();
+                foreach (int item in graph.adj[i]) // fill adjacentColors
+                    adjacentColors.AddLast(solutions[mutationIndex, item]);
+
+                if (adjacentColors.Contains(solutions[mutationIndex, i]))
+                {
+                    // mutate
+                    int newColor = rand.Next(allColors.Count);
+                    solutions[mutationIndex, i] = newColor;
+                }
+            }
+
+        }
+
+        public void letsGoGenetic(int amountChromosomes, int maxGenerations)
+        {
+            int generation = 0;
+            // void generatePopulation(int amount);
+            generatePopulation(amountChromosomes);
+
+            // void fitnessScore(int index);
+            for (int i = 0; i < amountChromosomes; i++)
+                fitnessScore(i);
+
+            while ((generation != maxGenerations) && (fitness.Max() != 0))
+            {
+                generation++;
+                Console.WriteLine("Generation " + generation);
+
+                // Tuple<int, int> parentSelect1();
+                // Tuple<int, int> parentSelect2();
+                Tuple<int, int> selection;
+                if (fitness.Max() > 5)
+                    selection = parentSelect1();
+                else
+                    selection = parentSelect2();
+
+                Console.WriteLine("Parents : " + selection.Item1 + " and " + selection.Item2);
+
+                // int[] crossover(int index1, int index2);
+                int[] child = crossover(selection.Item1, selection.Item2);
+
+                // int offspring(int[] result);
+                int index = offspring(child);
+
+                // void mutation1(int mutationIndex);
+                // void mutation2(int mutationIndex);
+                mutation1(index);
+
+                fitnessScore(index);
+                Console.Write("Min fitness == " + fitness.Min());
+                Console.Write(". Max fitness == " + fitness.Max());
+                Console.WriteLine(". New child on index " + index + "!!!!");
+            }
+            Console.WriteLine("Solution found in " + generation + ". generation!");
+            
+            int minColors = graph.V;
+            int minIndex = -1;
+            int colors;
+
+            for (int i = 0; i < solutions.GetLength(0); i++)
+            {
+                colors = -1;
+                if (fitness[i] == 0)
+                {
+                    Console.Write("Rozwiązanie[" + i + "] : ");
+                    for (int j = 0; j < solutions.GetLength(1); j++)
+                    {
+                        if (solutions[i,j] > colors)
+                            colors = solutions[i, j];
+                        Console.Write(solutions[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+                if (colors < minColors)
+                {
+                    minColors = colors;
+                    minIndex = i;
+                }
+            }
+
+            Console.WriteLine("Best is equal to " + (minColors+1) + " colors, on index " + minIndex);
+            
+        }
+    }
+
+}
--- OK-Project/Program.cs
+++ OK-Project/Program.cs
@@ -1,320 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace OK_Project
 {
-    class Graph
-    {
-        public int V;
-        public LinkedList<int>[] adj;
-        public int[] result;
-        public int fitness = 0;
-
-        // Constructor used to generate a random graph with given saturation
-        public Graph(int nodes, int saturation)
-        {
-            generate(nodes, saturation);
-        }
-
-        public Graph(string filename)
-        {
-            string line;
-            System.IO.StreamReader file = new System.IO.StreamReader(filename);
-            int v = int.Parse(file.ReadLine());
-
-            V = v;
-            result = new int[V];
-            adj = new LinkedList<int>[V];
-            for (int i = 0; i < v; i++)
-                adj[i] = new LinkedList<int>();
-
-
-            while ((line = file.ReadLine()) != null)
-            {
-                string[] val = line.Split(' ');
-                addEdge(int.Parse(val[0]) - 1, int.Parse(val[1]) - 1);
-            }
-        }
-
-        // Generate graph with given amount of nodes and saturation (in %)
-        public void generate(int nodes, int saturation)
-        {
-            V = nodes;
-            result = new int[V];
-            adj = new LinkedList<int>[V];
-
-            for (int i = 0; i < nodes; i++)
-                adj[i] = new LinkedList<int>();
-
-            var rnd = new Random();
-            var disconnected = new LinkedList<int>();
-            var connected = new LinkedList<int>();
-            int edges = 0;
-
-            for(int i = 0; i < nodes; i++)
-                disconnected.AddLast(i);
-
-            // Begin from random node
-            connected.AddLast(disconnected.ElementAt(rnd.Next(disconnected.Count())));
-            disconnected.Remove(connected.ElementAt(0));
-
-            // Loop generating base of the graph - connecting all necessary nodes together
-            while (disconnected.Count > 0)
-            {                
-                // pick 2 nodes - one connected, one not - and connect them
-                int node1 = disconnected.ElementAt(rnd.Next(disconnected.Count()));
-                int node2 = connected.ElementAt(rnd.Next(connected.Count()));
-
-                disconnected.Remove(node1);
-                connected.AddLast(node1);
-
-                addEdge(node1, node2);
-                edges++;
-            }
-
-            // Second part of graph generation - picking randomly two nodes
-            // and connecting them as long as the saturation requirement is not specified
-
-            while(edges < ((float) (nodes *  (nodes - 1)) / 2 * saturation / 100.0f))
-            {
-                int node1 = connected.ElementAt(rnd.Next(connected.Count()));
-                int node2 = -1;
-
-                Console.WriteLine("In second loop");
-
-                do
-                {
-                    node2 = connected.ElementAt(rnd.Next(connected.Count()));
-                    Console.WriteLine("In second loop - inner " + node2 + ", " + node1);
-                } while (node1 == node2);
-
-                if (!connection(node1, node2))
-                {
-                    addEdge(node1, node2);
-
-                    edges++;
-                }
-            }
-        }
-
-
-        public bool connection(int i, int j)
-        {
-            for(int xd = 0; xd < adj[i].Count(); xd++)
-            {
-                if (adj[i].ElementAt(xd) == j) return true;
-            }
-            return false;
-        }
-
-        public void print()
-        {
-            for(int i = 0; i < V; i++)
-            {
-                Console.Write(i + ": ");
-                for(int j = 0; j < adj[i].Count; j++)
-                {
-                    Console.Write(adj[i].ElementAt(j) + ", ");
-                }
-                Console.WriteLine();
-            }
-        }
-
-        public Graph clone()
-        {
-            Graph result = this;
-            return result;
-        }
-
-
-        public void addEdge(int v, int w)
-        {
-            adj[v].AddLast(w);
-            adj[w].AddLast(v);
-        }
-
-
-        /**
-         *  Algorytm zachłanny problemu kolorowania grafu.
-         *  Wierzchołki są kolorowane kolejno wg numeru wierzchołka.
-         *  @return colors - liczbę kolorów wykorzystanych do pokolorowania grafu w klasie.
-         *  Kolory i wierzchołki indeksowane od 0.
-         *  @return (max + 1).
-         */
-        public int greedy()
-        {
-            // Enumerable.Repeat<T>(x1, x2).ToArray() wypełnia tablicę wartościami x1, w ilości x2
-            bool[] avalible = Enumerable.Repeat<bool>(true, V).ToArray();
-
-            result[0] = 0;
-            
-            for (int i = 1; i < V; i++)
-            {
-                Console.WriteLine("i = " + i);
-                // checking for color's avalibility
-                foreach (int cr in adj[i])
-                {
-                    Console.WriteLine("cr = " + cr);
-                    Console.WriteLine("result[cr] = " + result[cr]);
-                    if (result[cr] != -1)
-                        avalible[result[cr]] = false;
-                }
-                
-                for (int cr = 0; cr < V; cr++)
-                    if (avalible[cr] == true)
-                    {
-                        result[i] = cr;
-                        break;
-                    }
-                avalible = Enumerable.Repeat<bool>(true, V).ToArray();
-            }
-
-            foreach (int i in result)
-                Console.WriteLine("Color : " + i);
-
-            return result.Max()+1;
-        }
-
-    }
-
-
-    class Genetics
-    {
-        Random rand = new Random();
-        public Graph[] chromosomes = new Graph[1];
-
-        List<LinkedList<int>> solutions;
-
-        public void generatePopulation(int amount)
-        {
-            solutions = new List<LinkedList<int>>();
-
-            int nodes = chromosomes[0].V;
-            int colours = 0;
-
-            for (int i = 0; i < nodes; i++)
-            {
-                if (chromosomes[0].adj[i].Count() > colours)
-                    colours = chromosomes[0].adj[i].Count();
-            }
-
-            for(int i = 0; i < amount; i++)
-            {
-                solutions.Add(new LinkedList<int>());
-
-                Console.Write("Rozwiązanie " + i + ": ");
-                for(int j = 0; j < nodes; j++)
-                {
-                    solutions.ElementAt(i).AddLast(rand.Next(colours));
-                    Console.Write(solutions.ElementAt(i).ElementAt(j) + ", ");
-                }
-                Console.WriteLine();
-            }
-
-
-        }
-        /**
-         * Function that calculates Fitness Score for given Graph
-         * Fitness == 0, means that we have perfect fitness
-         * In this case, as fitness score we used number of edges, that vertices have the same colour.
-         */
-        
-        
-        
-        public void fitnessScore(Graph graph)
-        {
-            for (int i = 0; i < graph.adj.Length; i++)
-            {
-                foreach (int j in graph.adj[i])
-                {
-                    // Iterates every element in every list
-                    if (graph.result[i] == graph.result[j])
-                        ++graph.fitness;
-                }
-            }
-            graph.fitness /= 2; // 'cuz calculates twice the same edge
-        }
-        
-        /**
-         * Function that select fittest parents from two random pairs of chromosomes.
-         * Used when best fitness greater than 4.
-         */
-        public Tuple<Graph, Graph> parentSelect1()
-        {
-            Graph g1, g2;
-
-            Graph tempPar1 = chromosomes[rand.Next(chromosomes.Length)];
-            Graph tempPar2 = chromosomes[rand.Next(chromosomes.Length)];
-
-            // ??????
-            // not sure, if should be greater or less
-            // ??????
-            if (tempPar1.fitness > tempPar2.fitness)
-                g1 = tempPar1; 
-            else
-                g1 = tempPar2;
-
-            tempPar1 = chromosomes[rand.Next(chromosomes.Length)];
-            tempPar2 = chromosomes[rand.Next(chromosomes.Length)];
-
-            if (tempPar1.fitness > tempPar2.fitness)
-                g2 = tempPar1;
-            else
-                g2 = tempPar2;
-
-            return new Tuple<Graph, Graph>(g1, g2);
-        }
-
-        /**
-        * Function that select two best performing chromosomes
-        * Used when best fitness less than 4.
-        */
-        public Tuple<Graph, Graph> parentSelect2()
-        {
-            Graph g1, g2;
-            g1 = chromosomes[0];
-            g2 = chromosomes[0];
-
-            for (int i = 1; i < chromosomes.Length; i++)
-                if (g1.fitness > chromosomes[i].fitness)
-                    g1 = chromosomes[i];
-
-            for (int i = 1; i < chromosomes.Length; i++)
-                if ((g2.fitness > chromosomes[i].fitness) && (g1 != chromosomes[i]))
-                    g2 = chromosomes[i];
-            
-            return new Tuple<Graph, Graph>(g1, g2);
-        }
-
-        /**
-         * Funcion that cross two parents into one child.
-         * I like to call it "reproduction".
-         * Crosspoint is generated at random.
-         */
-        public Graph crossover(Graph g1, Graph g2)
-        {
-            Graph child = g1;
-            int crosspoint = rand.Next(child.V);
-
-            for (int i = 0; i <= crosspoint; i++)
-                child.result[i] = g1.result[i];
-
-            for (int i = crosspoint + 1; i <= child.V; i++)
-                child.result[i] = g2.result[i];
-
-            return child;
-        }
-
-    }
-    
     class Program
     {
+        /*
+         * TO DO:
+         * * check when we need to recalculate Fitness Score.
+         * 
+         */
+
         static void Main(string[] args)
         {
+            
             // Graph g = new Graph("test.txt");
-            Graph g = new Graph(9, 10);
-            Graph test = g.clone();
+            Graph g = new Graph(30, 70);
 
             // g.print();
 
@@ -322,13 +26,10 @@ namespace OK_Project
             //Console.WriteLine("Test = " + test.result.Max());
 
             Genetics nowy = new Genetics();
-            nowy.chromosomes[0] = g;
-            nowy.generatePopulation(5);
-            //nowy.fitnessScore(test);
-            //Tuple<Graph, Graph> xxx = nowy.parentSelect1();
+            nowy.graph = g;
+            nowy.letsGoGenetic(50, 100);
 
-            //xxx = nowy.parentSelect1();
-            //xxx = nowy.parentSelect1();
+            Console.WriteLine("Zachłanny algorytm: " + g.greedy());
 
             Console.ReadKey();
         }
